7d2230dc2b03b08b5a2ac3b63470d4f0e593318a
He Quanjie Boey <73111160+hqjb91@users.noreply.github.com>
2026-01-08 13:47:53 +0800

    Add Sample3 EF Core in-memory sample

---
+++ src/Sample3/Program.cs
@@ -0,0 +1,37 @@
+using GvatarWorkflow;
+using GvatarWorkflow.Providers;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<WorkflowDbContext>(options =>
+    options.UseInMemoryDatabase("WorkflowDb"));
+
+builder.Services.AddGvatarWorkflow(opt =>
+{
+    opt.UsePersistenceProvider(serviceProvider =>
+        new EntityFrameworkPersistenceProvider(serviceProvider.GetRequiredService<WorkflowDbContext>()));
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
